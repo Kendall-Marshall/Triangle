219cf7eb482a482c913d4a831fa8d855bb72e9ab
Kendall-Marshall <36905603+Kendall-Marshall@users.noreply.github.com>
2018-02-28 22:50:09 +1300

    Bombs Present in code

---
+++ Triangle/Assets/Scripts/BombMvmt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BombMvmt : MonoBehaviour {
+	public GameObject Player;
+	public static bool check = false;
+	// Use this for initialization
+	void Start () {
+
+		if (check == true) {
+			StartCoroutine (BombScale ());
+		}
+	}
+	
+	// Update is called once per frame
+	IEnumerator BombScale(){
+		float currentTime = 0.0f;
+
+		do
+		{
+
+			if(MainScript.runCheck == true){
+				Destroy(Player);
+				break;
+			}
+
+			currentTime += Time.deltaTime;
+			yield return null;
+		} while (currentTime <= 2);
+
+		Player.SetActive (false);
+		Destroy(Player);
+	}
+}
--- Triangle/Assets/Scripts/MainScript.cs
+++ Triangle/Assets/Scripts/MainScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour {
 	public GameObject obj;
+	public GameObject bomb;
 	public Button but;
 	public GameObject Hide;
 	public GameObject Enable;
@@ -31,9 +32,11 @@ public class MainScript : MonoBehaviour {
 		while(runCheck == false){
 			yield return new WaitForSeconds (level);
 			Vector3 position = new Vector3 (Random.Range (-2.2f, 2.2f), Random.Range (-4.5f, 2.7f), 0);
+			Vector3 bombPos = new Vector3 (Random.Range (-2.2f, 2.2f), Random.Range (-4.5f, 2.7f), 0);
 			MovementControl.check = true;
-
+			BombMvmt.check = true;
 			Instantiate (obj, position, Quaternion.identity);
+			Instantiate (bomb, bombPos, Quaternion.identity);
 
 			count++;
 			if (count == 5) {
