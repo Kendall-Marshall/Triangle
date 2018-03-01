cfb6b243bab42354921ec27d572a623113220eb8
Kendall-Marshall <36905603+Kendall-Marshall@users.noreply.github.com>
2018-03-01 19:29:28 +1300

    Got bombs to show up

---
+++ Triangle/Assets/Scripts/BombClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BombClick : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void OnMouseDown() {
+		if (MainScript.runCheck == false) {
+			//MainScript.runCheck = true;
+			BombMvmt.BombClickCheck = true;
+			//Destroy (gameObject);
+		}
+	}
+}
--- Triangle/Assets/Scripts/BombMvmt.cs
+++ Triangle/Assets/Scripts/BombMvmt.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class BombMvmt : MonoBehaviour {
 	public GameObject Player;
+	public Sprite LossSprite;
 	public static bool check = false;
-	// Use this for initialization
-	void Start () {
+	public static bool BombClickCheck = false;
 
+	private SpriteRenderer spr;
+
+	void Start () {
+		spr = GetComponent<SpriteRenderer>();
 		if (check == true) {
 			StartCoroutine (BombScale ());
 		}
@@ -26,10 +30,37 @@ public class BombMvmt : MonoBehaviour {
 				break;
 			}
 
+			if(BombClickCheck == true){
+				
+				StartCoroutine(BombDeath());
+				break;
+			}
+
 			currentTime += Time.deltaTime;
 			yield return null;
 		} while (currentTime <= 2);
 
+		if (BombClickCheck == false) {
+			Player.SetActive (false);
+			Destroy (Player);
+		}
+
+	}
+
+	IEnumerator BombDeath(){
+		MainScript.runCheck = true;
+		float currentTime = 0.0f;
+		spr.sprite = LossSprite;
+		do
+		{
+			Debug.Log ("1234");
+
+
+			currentTime += Time.deltaTime;
+			yield return null;
+		} while (currentTime <= 5);
+
+		//MainScript.runCheck = true;
 		Player.SetActive (false);
 		Destroy(Player);
 	}
