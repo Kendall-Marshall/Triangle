538a0eb60e823e6a2ccf9258801f6d96693d0de2
Kendall-Marshall <36905603+Kendall-Marshall@users.noreply.github.com>
2018-03-21 21:20:52 +1300

    Close Now

--- Triangle/Assets/Scripts/BombClick.cs
+++ Triangle/Assets/Scripts/BombClick.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class BombClick : MonoBehaviour {
 	void OnMouseDown() {
 		if (MainScript.runCheck == false) {
-			BombMvmt.BombClickCheck = true;
-		}
+            BombMvmt.BombClickCheck = true;
+        }
 	}
 }
